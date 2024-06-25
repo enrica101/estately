23ee622659c6eac955d611e52dc73b315791e053
Enrica Carpena <enricajcarpena@gmail.com>
2024-06-25 11:32:25 +0800

    Integrated TailwindCSS and created init files - working

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className='text-lime-400'>
+          This is a text
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
